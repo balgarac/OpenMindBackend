6e2bc593b5279fb5e6f2f67b1c87b047e3b250f9
Gangyeon Jo <gangyeonjo336@gmail.com>
2019-07-20 00:49:14 +0900

    버그픽스중.

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -62,31 +62,26 @@ exports.listenerSignup = (req, res) => {
 };
 
 exports.talkerSignin = (req, res) => {
-  const id = req.params.id;
-  const pw = req.params.pw;
+  const id = req.body.id;
+  const pw = req.body.pw;
+  console.log(id, pw);
 
-  var talker = [];
-  const SELECT_TALKER = 'SELECT * FROM talker';
+  const SELECT_TALKER = `SELECT * FROM talker WHERE id = ${id}`;
   connection.query(SELECT_TALKER, (error, rows) => {
-    if (error) console.log(error);
-    for (var i in rows) 
-      talker.push(rows[i]);
-  });
-
-  const talkerUser = talker.filter(talker => {
-    (talker.id === id) && (talker.pw === pw)
-  })[0];
-  const UserToken = {
-    token: talkerUser.token
-  };
-
-  if (!talkerUser) {
-    return res.status(401).send("Login failed");
-  }
-  return res.status(200).json(UserToken).send("Signup success!");
+    if(error){
+        console.log(error); //에러가 있다면 에러를 console에 출력.
+    }
+    for(var i in rows){
+        if(rows[i].password == pw){
+            res.status(200).json({token: rows[i].token});
+        }
+        else res.status(401).send('signin fail!');
+    }
+  })
 };
 
 exports.talkerSignup = (req, res) => {
+    console.log(1);
     const name = req.body.name;
     const phone = req.body.phone;
     const id = req.body.id;
@@ -100,6 +95,7 @@ exports.talkerSignup = (req, res) => {
     const INSERT_TALKER = `INSERT INTO listener (name, phoneNumber, id, pw, age, sex, field, area, token) VALUES ('${name}', '${phone}', '${id}', '${pw}', '${age}', '${sex}', '${field}', '${area}', '${token}')`
     
     connection.query(CHECK_DUP, (rows) => {
+        console.log(2);
         for(var i in rows){
             if(rows[i].id == id){
                 return res.status(400).send('Signup fail!');
@@ -107,8 +103,31 @@ exports.talkerSignup = (req, res) => {
             else{
                 connection.query(INSERT_TALKER, () => {
                     return res.status(200).send('Signup success!');
-                })
+                });
             }
         }
     })
 };
+
+const smsVerification = (req, res) => {
+    const phoneNumber = req.body.phoneNumber;
+  
+    request({
+      method: 'POST',
+      json: true,
+      uri: `https://api-sens.ncloud.com/v1/sms/services/${process.env.SENS_SERVICEID}/messages`,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-NCP-auth-key': process.env.SENS_ACCESSKEYID,
+        'X-NCP-service-secret': process.env.SENS_SERVICESECRET
+      },
+      body: {
+        type: 'sms',
+        from: process.env.SENS_SENDNUMBER,
+        to: [`${phoneNumber}`],
+        content: `WeGoing 인증번호 ${verificationNumber}입니다.`
+      }
+    });
+  
+    return res.json({ phoneNumber: phoneNumber });
+  }
--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', require('../api/listener/index'));
 app.use('/', require('../api/tags/index'));
