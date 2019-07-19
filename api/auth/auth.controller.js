d23c8c4d5a70a5a95a40e189c61707e75ba5c0ee
조웅연 <lockhost23@gmail.com>
2019-07-19 23:42:02 +0900

    로그인 라우팅 모듈 구현

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,7 +1,28 @@
 const connection = require('../../DB/connection');
 
 exports.listenerSignin = (req, res) => {
-  
+  const id = req.params.id;
+  const pw = req.params.pw;
+
+  var listeners = [];
+  const SELECT_LISTENER = 'SELECT * FROM listener';
+  connection.query(SELECT_LISTENER, (error, rows) => {
+    if (error) console.log(error);
+    for (var i in rows) 
+      listeners.push(rows[i]);
+  });
+
+  const listenUser = listeners.filter(listener => {
+    (listener.id === id) && (listener.pw === pw)
+  })[0];
+  const UserToken = {
+    token: listenUser.token
+  };
+
+  if (!listenUser) {
+    return res.status(401).send("Login failed");
+  }
+  return res.status(200).json(UserToken).send("Signup success!");
 };
 
 exports.listenerSignup = (req, res) => {
@@ -9,7 +30,28 @@ exports.listenerSignup = (req, res) => {
 };
 
 exports.talkerSignin = (req, res) => {
+  const id = req.params.id;
+  const pw = req.params.pw;
 
+  var talker = [];
+  const SELECT_TALKER = 'SELECT * FROM talker';
+  connection.query(SELECT_TALKER, (error, rows) => {
+    if (error) console.log(error);
+    for (var i in rows) 
+      talker.push(rows[i]);
+  });
+
+  const talkerUser = talker.filter(talker => {
+    (talker.id === id) && (talker.pw === pw)
+  })[0];
+  const UserToken = {
+    token: talkerUser.token
+  };
+
+  if (!talkerUser) {
+    return res.status(401).send("Login failed");
+  }
+  return res.status(200).json(UserToken).send("Signup success!");
 };
 
 exports.talkerSignup = (req, res) => {
