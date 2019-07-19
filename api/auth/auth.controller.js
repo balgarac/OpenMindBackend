04cbb9d524b79ddae877444e50a7cf5d4e30a4a7
조웅연 <lockhost23@gmail.com>
2019-07-19 22:43:17 +0900

    로그인, 회원가입 라우팅 모듈 추가

---
+++ api/auth/auth.controller.js
@@ -0,0 +1,17 @@
+const connection = require('../../DB/connection');
+
+exports.listenerSignin = (req, res) => {
+  
+};
+
+exports.listenerSignup = (req, res) => {
+
+};
+
+exports.talkerSignin = (req, res) => {
+
+};
+
+exports.talkerSignup = (req, res) => {
+
+};
---
+++ api/auth/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('./auth.controller');
+
+router.get('/listener/signin', );
+router.get('/listener/signup', );
+router.get('/talker/signin', );
+router.get('/talker/signup', );
+
+module.exports = router;
