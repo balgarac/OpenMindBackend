6d2ebfe6dedd422742f151bda15bdc05e0945429
조웅연 <lockhost23@gmail.com>
2019-07-20 07:05:39 +0900

    쓰레기 코드 삭제

--- api/auth/index.js
+++ api/auth/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('./auth.controller');
-const phoneAuthController = require('./phone.auth.controller');
 
 router.post('/listener/signin', controller.listenerSignin);
 router.post('/listener/signup', controller.listenerSignup);
