e19a7325132bb5d092663caa5c48aa7b10893908
조웅연 <lockhost23@gmail.com>
2019-07-19 19:05:12 +0900

    mysql 커넥션 코드 모듈 추가

--- DB/connection.js
+++ DB/connection.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host : "13.124.148.2",
+    host : "127.0.0.1",
     port : 3306,
     user : "root",
     password : "1111",
     database : "openMind"
 });
 
-connection.connect();
-
-connection.query(`insert into tags (tag) values ('11111')`);
+module.exports = connection;
